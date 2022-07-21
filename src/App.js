69cda12316c87da5f839a21f60aa87573586a358
flodalia3 <florinda.dalia3@gmail.com>
2022-07-21 17:37:51 +0200

    add map

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   // const setName = nameState[1];
   const [name, setName] = useState("Ciccio");
   const [surname, setSurname] = useState("Pasticcio");
+  const [courses, setCourses] = useState([]);
   //let name = "Ciccio";
   //let surname = "Pasticcio";
   const mainSearch = (n) => console.log(n);
@@ -19,16 +20,44 @@ function App() {
   };
   const loadData = () => {
     axios.get("http://localhost:8080/course")
-    .then((res) => console.log(res))
+    .then((res) => setCourses(res.data))
   };
- 
+
   return (
     <div className="App">
       <p>Hello {name} {surname}!</p>
       <button onClick={changeData}>Click Me</button>
       <button onClick={loadData}>Load Data from Server</button>
       <MyComponent clientName={name} goldUser="true" onSubmit={mainSearch} />
-      
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Hourse Course</th>
+            <th>Level Course</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            courses.map( (c,p) => {
+              return (
+                <tr key={p}>
+                  <td>{c.id}</td>
+                  <td>{c.title}</td>
+                  <td>{c.hoursCourse}</td>
+                  <td>{c.courseLevel}</td>
+                  <td>{c.description}</td>
+                  <td>{c.price}</td>
+                </tr>
+              )
+            }
+            )
+          }
+        </tbody>
+      </table>
       
     </div>
   );
